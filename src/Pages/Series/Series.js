fbce860af51515530fec5c25856551bcc0c43d2c
Namit Malasi <namitmalasi@gmail.com>
2021-09-15 17:32:17 +0530

    created the basic layout of the project

---
+++ src/Pages/Series/Series.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Series = () => {
+  return (
+    <div>
+      <span className="" pageTitle>
+        TV Series
+      </span>
+    </div>
+  );
+};
+
+export default Series;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <span className="header">🎬movie hub🎥</span>
+    </div>
+  );
+};
+
+export default Header;
